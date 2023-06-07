473126a4f08df92e8d5970edd71b5b75d559846c
German Cano <130206071+canogerman@users.noreply.github.com>
2023-06-07 00:55:12 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const form = document.querySelector("form");
 const firstNameInput = document.getElementById("firstname");
 const lastNameInput = document.getElementById("lastname");
 const userNameInput = document.getElementById("username");
@@ -7,83 +8,71 @@ const birthdayInput = document.getElementById("birthday");
 
 firstNameInput.addEventListener("input", function (event) {
   const value = event.target.value;
-
+  const regExp = new RegExp(/^[A-Za-z]+$/);
   // Validate that only letters are allowed
-  if (!/^[A-Za-z]+$/.test(value)) {
+  if (!regExp.test(value)) {
     event.target.setCustomValidity("Please enter only letters");
-  } else {
-    // Validate the maximum length
-    if (value.length > 20) {
-      event.target.setCustomValidity(
-        "The first name must have a maximum of 20 characters"
-      );
-    } else {
-      event.target.setCustomValidity(""); // Clear custom error message if validation passes
-    }
   }
+  // Validate the maximum length
+  if (value.length > 20) {
+    event.target.setCustomValidity(
+      "The first name must have a maximum of 20 characters"
+    );
+  }
+  event.target.setCustomValidity(""); // Clear custom error message if validation passes
 });
-
-// It is clear that here I must unify firstname and lastname in a single function 😓.
 
 lastNameInput.addEventListener("input", function (event) {
   const value = event.target.value;
-
+  const regExp = new RegExp(/^[A-Za-z]+$/);
   // Validate that only letters are allowed
-  if (!/^[A-Za-z]+$/.test(value)) {
+  if (!regExp.test(value)) {
     event.target.setCustomValidity("Please enter only letters");
-  } else {
-    // Validate the maximum length
-    if (value.length > 20) {
-      event.target.setCustomValidity(
-        "The last name must have a maximum of 20 characters"
-      );
-    } else {
-      event.target.setCustomValidity("");
-    }
   }
+  // Validate the maximum length
+  if (value.length > 20) {
+    event.target.setCustomValidity(
+      "The last name must have a maximum of 20 characters"
+    );
+  }
+  event.target.setCustomValidity("");
 });
 
 userNameInput.addEventListener("input", function (event) {
   const value = event.target.value;
-
+  const regExp = new RegExp(/^[A-Za-z0-9]+$/);
   // Validate that only letters and numbers are allowed
-  if (!/^[A-Za-z0-9]+$/.test(value)) {
+  if (!regExp.test(value)) {
     event.target.setCustomValidity("Please enter only letters and numbers");
-  } else {
-    // Validate the maximum length
-    if (value.length > 10) {
-      event.target.setCustomValidity(
-        "The user name must have a maximum of 10 characters"
-      );
-    } else {
-      event.target.setCustomValidity("");
-    }
   }
+  // Validate the maximum length
+  if (value.length > 10) {
+    event.target.setCustomValidity(
+      "The user name must have a maximum of 10 characters"
+    );
+  }
+  event.target.setCustomValidity("");
 });
 
 passwordInput.addEventListener("input", function (event) {
   const value = event.target.value;
-
+  const regExp = new RegExp(/^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8}$/);
   // Validate that 8 character are allowed whit uppercase and numbers.
-  if (!/^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8}$/.test(value)) {
+  if (!regExp.test(value)) {
     event.target.setCustomValidity(
       "Password must be 8 characters. at least 1 must be uppercase and at least 1 must be a number"
     );
-  } else {
-    event.target.setCustomValidity("");
   }
+  event.target.setCustomValidity("");
 });
 
 passwordConfirmInput.addEventListener("input", function (event) {
   const value = event.target.value;
   if (passwordInput.value !== value) {
     event.target.setCustomValidity("The password does not match");
-  } else {
-    event.target.setCustomValidity("");
   }
+  event.target.setCustomValidity("");
 });
-
-const form = document.querySelector("form");
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
@@ -104,7 +93,5 @@ form.addEventListener("submit", function (event) {
   if (confirm(confirmationMessage)) {
     // User confirmed, submit the form
     form.submit();
-  } else {
-    // User cancelled, do nothing
   }
 });
