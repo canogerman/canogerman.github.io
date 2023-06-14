e0e7ee02f56f3d4eada57acaf15cdf71192684d7
German Cano <130206071+canogerman@users.noreply.github.com>
2023-06-14 18:19:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".loader");
+  loader.style.opacity = 0;
+  loader.style.visibility = "hidden";  
+  const loaderText = document.querySelector(".loader-text");
+  loaderText.style.opacity = 0;
+  loaderText.style.visibility = "hidden";
+
+});
